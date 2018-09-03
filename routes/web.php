20a42887580cb69c8b2ba2af56f20fdb20152948
Amadeusz Annissimo <annissimo@gmail.com>
2018-09-03 08:58:07 -0400

    Require email confirmation for new thread page - resolves #2

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::view('scan', 'scan');
 
 Route::get('/threads', 'ThreadController@index')->name('threads');
-Route::get('/threads/create', 'ThreadController@create');
+Route::get('/threads/create', 'ThreadController@create')->middleware('must-be-confirmed');
 Route::get('/threads/search', 'SearchController@show');
 Route::delete('/threads/{channel}/{thread}', 'ThreadController@destroy');
 Route::patch('/threads/{channel}/{thread}', 'ThreadController@update');
